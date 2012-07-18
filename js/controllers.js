4ebd64392433a3e444004bd57d8ccaf2dd9b078f
Jordi Nebot <llengot@gmail.com>
2012-07-18 13:08:08 +0200

    Removed appCtrl

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,15 @@
 'use strict';
 
-function appCtrl($scope, $http, $routeParams) {
+function homeCtrl($scope, $http, $routeParams) {
 	var actual_tiles = 10;
 	var actual_width = 240;
 	var actual_height = 500;
 	
 	init_tiles(actual_tiles, actual_width, actual_height);	
-}
-
-function homeCtrl($scope, $http, $routeParams) {
-	
 	
 	$scope.tiles = tiles;
 	
 	// Arrange tiles when ready
 	$scope.$on('$viewContentLoaded', arrange_tiles);
-	
-	
 
 }
