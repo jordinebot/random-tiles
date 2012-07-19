e9a2c4c207c33e4253bffd57a340c997f39df468
Jordi Nebot <llengot@gmail.com>
2012-07-19 13:35:53 +0200

    JQuery actions disabled

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 'use strict';
 
-angular.module('tiles', []).
-  config(['$routeProvider', function($routeProvider) {
+/**
+ * Main App Module 
+ */
+angular.module('tiles', []).directive('myAwesomeDirective', function() {
+    return {
+        // directive definition object goes here
+    };
+}).config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'partials/home.html', controller: homeCtrl}).
       otherwise({redirectTo: '/'});
